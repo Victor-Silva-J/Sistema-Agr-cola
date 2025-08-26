fc665a00162a1a2ce20247d70e9c0da5c51db5c1
Victor Silva <admvictor24@gmail.com>
2025-08-26 20:39:39 -0300

    Implementação do método para excluír produtos.

--- src/main/java/com/gestaoagricola/controleagricola/controllers/EstoqueController.java
+++ src/main/java/com/gestaoagricola/controleagricola/controllers/EstoqueController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +49,13 @@ public class EstoqueController {
 		return ResponseEntity.ok(lista);
 	}
 	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<Void> excluir(@PathVariable Long id){
+		repository.deleteById(id);
+		
+		return ResponseEntity.noContent().build();
+	}
 	
 	
 }
